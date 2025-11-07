a5e2332beb433722d8f0fc6840085e1e2ab31117
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-07 11:09:09 -0800

    made new game

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,19 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        prepare();
+        
+    }
+    private void prepare(){
+    
+        frog frog = new frog();
+        addObject(frog, 300, 200);
+        
+        for(int i = 0; i < 5; i++){
+        bug bug = new bug();
+        addObject(bug, Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(400));
+        } 
     }
 }
+    
+
