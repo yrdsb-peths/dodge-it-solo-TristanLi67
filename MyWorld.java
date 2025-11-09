23641c072421fcefeabe155ba640d1154b9ae106
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-09 14:51:47 -0800

    added a score system
    added score

--- MyWorld.java
+++ MyWorld.java
@@ -1,20 +1,29 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int score = 0;
+    private int spawnTimer = 0;
+
     public MyWorld() {
         super(600, 400, 1);
         prepare();
-        
     }
-    private void prepare(){
-    
-        frog frog = new frog();
-        addObject(frog, 300, 200);
-        
-        for(int i = 0; i < 5; i++){
-        bug bug = new bug();
-        addObject(bug, Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(400));
-        } 
+
+    private void prepare() {
+        addObject(new Frog(), 300, 200);
+    }
+
+    public void act() {
+        spawnTimer++;
+        if (spawnTimer >= 150) { // Spawns less often
+            addObject(new Bug(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(400));
+            spawnTimer = 0;
+        }
+    }
+
+    public void increaseScore() {
+        score++;
+        showText("Score: " + score, 70, 30);
     }
 }
     
--- frog.java
+++ frog.java
@@ -6,30 +6,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (Tristan) 
  * @version (a version number or a date)
  */
-public class frog extends Actor
-{
-    /**
-     * Act - do whatever the frog wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        moveAround();
-        checkForBug();// Add your action code here.
+
+public class Frog extends Actor {
+    public void act() {
+        moveFrog();
+        checkCollision();
     }
-    private void moveAround() 
-    {
-        if(Greenfoot.isKeyDown("left")) move(-4);
-        if(Greenfoot.isKeyDown("right")) move(4);
-        if(Greenfoot.isKeyDown("up")) setLocation(getX(),getY() -4);
-        if(Greenfoot.isKeyDown("down")) setLocation(getX(),getX() +4);
+
+    private void moveFrog() {
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 4);
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 4);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 4, getY());
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 4, getY());
+        }
     }
-    private void checkForBug()
-    {
-        Actor bug = getOneIntersectingObject(bug.class);
-        if (bug != null)
-        {
+
+    private void checkCollision() {
+        Bug bug = (Bug)getOneIntersectingObject(Bug.class);
+        if (bug != null) {
             getWorld().removeObject(bug);
+            MyWorld world = (MyWorld)getWorld();
+            world.increaseScore();
         }
     }
 }
