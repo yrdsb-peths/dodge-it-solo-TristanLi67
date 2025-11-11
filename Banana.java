b3c445959d2a10631d6036a59e660a315b656370
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-11 11:17:22 -0800

    added banana

--- Banana.java
+++ Banana.java
@@ -14,6 +14,19 @@ public class Banana extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        move(-3);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }// Add your action code here.
+    }
+    
+    public void resetBanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
