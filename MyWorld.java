3e2b471c103ebf998c4e8a9b0a027dfe99b6b3e5
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-11 11:04:43 -0800

    added hero class

--- MyWorld.java
+++ MyWorld.java
@@ -1,40 +1,24 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class MyWorld extends World {
-    private int score = 0;
-    private static int highScore = 0; 
-    private int spawnTimer = 0;
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
 
-    public MyWorld() {
-        super(600, 400, 1);
-        prepare();
-        updateScoreboard();
-    }
-
-    private void prepare() {
-        addObject(new Frog(), 300, 200);
-    }
-
-    public void act() {
-        spawnTimer++;
-
-        if (spawnTimer >= 250) { 
-            addObject(new Bug(), Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(400));
-            spawnTimer = 0;
-        }
-    }
-
-    public void increaseScore() {
-        score++;
-        if (score > highScore) {
-            highScore = score;
-        }
-        updateScoreboard();
-    }
-
-    private void updateScoreboard() {
-        showText("Score: " + score, 70, 30);
-        showText("High Score: " + highScore, 500, 30);
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
     }
 }
-    
